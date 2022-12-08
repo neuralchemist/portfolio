f6f276592320a3fe3aeefb6ba6bc30bd3d81a60f
neuralchemist <hacker.turab91@gmail.com>
2022-12-08 15:22:27 +0100

    feat(skillsTab) added more skill icons

--- src/components/SkillsTab/data/EnvStackData.tsx
+++ src/components/SkillsTab/data/EnvStackData.tsx
@@ -1,9 +1,19 @@
 // node
 import { ReactComponent as NodejsIcon } from "../../../assets/svg/nodejs-icon.svg";
+import { ReactComponent as RestApiIcon } from "../../../assets/svg/restful-api.svg";
+import { ReactComponent as GraphqlIcon } from "../../../assets/svg/graphql.svg";
 
 export const EnvStackData = [
   {
     name: "Nodejs",
     icon: <NodejsIcon width="100%" height="100%" />,
+  },
+  {
+    name: "Rest Api",
+    icon: <RestApiIcon width="100%" height="100%" />,
+  },
+  {
+    name: "Graphql",
+    icon: <GraphqlIcon width="100%" height="100%" />,
   },
 ];
--- src/components/SkillsTab/data/TestingStackData.tsx
+++ src/components/SkillsTab/data/TestingStackData.tsx
@@ -1,9 +1,14 @@
 // testing
 import { ReactComponent as CypressIcon } from "../../../assets/svg/cypress-icon.svg";
+import { ReactComponent as JestIcon } from "../../../assets/svg/jest.svg";
 
 export const TestingStackData = [
   {
     name: "Cypress",
     icon: <CypressIcon width="100%" height="100%" />,
+  },
+  {
+    name: "Jest",
+    icon: <JestIcon width="100%" height="100%" />,
   },
 ];
